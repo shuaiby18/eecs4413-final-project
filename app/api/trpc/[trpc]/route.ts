c26dcdf6abc0875219dcce253b19b0e0182322dd
Saiz Prasla <saiz01@hotmail.com>
2024-08-27 19:47:48 -0400

    fix trpc

--- app/api/trpc/[trpc]/route.ts
+++ app/api/trpc/[trpc]/route.ts
@@ -1,7 +1,11 @@
+//Importing the feth request handlers from the TRPC fetch pakage
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 
+//imorting app router to help define routes 
 import { appRouter } from "@/server";
 
+
+//Responsible for handling POST and GET requests 
 const handler = (req: Request) =>
   fetchRequestHandler({
     endpoint: "/api/trpc",
@@ -10,4 +14,5 @@ const handler = (req: Request) =>
     createContext: () => ({}),
   });
 
+//Eporting created handler to be used for POST and GET requests so that it can be used with TRPC
 export { handler as GET, handler as POST }
