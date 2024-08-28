34975052704571df69bc8d22c6df424f4406cab3
shuaiby18 <shuaib18@my.yorku.ca>
2024-08-28 02:26:58 -0400

    Vercel Fix

--- lib/schemas/user.ts
+++ lib/schemas/user.ts
@@ -1,18 +1,21 @@
 import { z } from "zod";
 
 export const LoginSchema = z.object({
-    email: z.string().email({
-      message: "Please enter a valid email address. Email is required.",
-    }),
-    password: z.string().min(1, {
-      message: "Please enter your password. Password is required.",
-    }),
-    code: z.optional(z.string()),
-    callbackUrl: z.string().default("/"),
-  });
-  
+  email: z.string().email({
+    message: "Please enter a valid email address. Email is required.",
+  }),
+  password: z.string().min(1, {
+    message: "Please enter your password. Password is required.",
+  }),
+  code: z.optional(z.string()),
+  callbackUrl: z.string().default("/"),
+});
+
 export const RegisterSchema = z
   .object({
+    name: z.string().min(1, {
+      message: "Please enter your name, required.",
+    }), // Add this line
     email: z.string().email({
       message: "Please enter a valid email address, required.",
     }),
@@ -28,7 +31,7 @@ export const RegisterSchema = z
     path: ["passwordConfirmation"],
   });
 
-  export const UpdateSchema = z
+export const UpdateSchema = z
   .object({
     name: z.string().min(3, {
       message: "Please enter name, required",
@@ -36,16 +39,20 @@ export const RegisterSchema = z
     email: z.string().email({
       message: "Please enter a valid email address, required.",
     }),
-    password: z.string().min(6, {
-      message: "Please enter a password with at least 6 characters, required",
-    }).or(z.literal("")),
-    passwordConfirmation: z.string().min(6, {
-      message: "Please confirm your password, required.",
-    }).or(z.literal("")),
+    password: z
+      .string()
+      .min(6, {
+        message: "Please enter a password with at least 6 characters, required",
+      })
+      .or(z.literal("")),
+    passwordConfirmation: z
+      .string()
+      .min(6, {
+        message: "Please confirm your password, required.",
+      })
+      .or(z.literal("")),
   })
   .refine((data) => data.password === data.passwordConfirmation, {
     message: "Passwords do not match.",
     path: ["passwordConfirmation"],
   });
-
-  
